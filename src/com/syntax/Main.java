726613ef32b7c3feb2a17aec3fd1679c04ba60a7
Petar <99811846+petarxiv@users.noreply.github.com>
2022-05-17 19:17:13 -0400

    Solar System Basic ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,14 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SolarSystem FactorX=new SolarSystem();
+        FactorX.Sun="1";
+        FactorX.Moon="4";
+        FactorX.Star="2";
+        FactorX.Planet="1000";
+        
+    }
+}
---
+++ src/com/syntax/SolarSystem.java
@@ -0,0 +1,9 @@
+package com.syntax;
+
+public class SolarSystem {
+    String Sun;
+    String Moon;
+    String Star;
+    String Planet;
+
+}
