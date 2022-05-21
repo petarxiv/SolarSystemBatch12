255ef626c291750d274be549b8b2b4528caf5840
Petar <99811846+petarxiv@users.noreply.github.com>
2022-05-21 00:38:57 -0400

    new changes

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,7 +3,7 @@ package com.syntax;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         SolarSystem FactorX=new SolarSystem();
         FactorX.Sun="1";
         FactorX.Moon="4";
@@ -15,6 +15,12 @@ public class Main {
         Xfactor.color="green";
         Xfactor.radius="4546";
         Xfactor.heat="2345657yt";
+
+        //adding the feature for star in the solar system
+        FeatureStars Facx=new FeatureStars();
+        Facx.color="green";
+        Facx.radius="432758gd978";
+        Facx.size="999999m";
 
 
     }
