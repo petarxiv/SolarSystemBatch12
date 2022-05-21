98a00b97d2c3c8933370d5283b3512d566697fa0
Petar <99811846+petarxiv@users.noreply.github.com>
2022-05-21 00:20:08 -0400

    planet2 has been added succesfully

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -1,9 +1,11 @@
 package com.syntax;
 
+import sun.misc.OSEnvironment;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         SolarSystem FactorX=new SolarSystem();
         FactorX.Sun="1";
         FactorX.Moon="4";
@@ -15,6 +17,18 @@ public class Main {
         Xfactor.color="green";
         Xfactor.radius="4546";
         Xfactor.heat="2345657yt";
+
+        //adding the feature for star in the solar system
+        FeatureStars Facx=new FeatureStars();
+        Facx.color="green";
+        Facx.radius="432758gd978";
+        Facx.size="999999m";
+
+        //adding features for planet2
+        Planet2 Olso=new Planet2();
+        Olso.name="Ozhan";
+        Olso.size="76583km";
+        Olso.color="pink";
 
 
     }
---
+++ src/com/syntax/Planet2.java
@@ -0,0 +1,9 @@
+package com.syntax;
+
+public class Planet2 {
+
+    String color;
+    String name;
+    String size;
+
+}
